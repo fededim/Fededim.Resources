7adc2e018f82c1af2a78d1bec257f37403be4cb1
Federico Di Marco <fededim@gmail.com>
2025-03-04 19:26:42 +0100

    added color

--- PowershellResources/IndexNowIndexWebsites.ps1
+++ PowershellResources/IndexNowIndexWebsites.ps1
@@ -5,7 +5,7 @@ Utility to perform indexing of webpages through IndexNow of one or more domains 
 
 .DESCRIPTION
 
-You must specify the domain name and the indexNow key in the input hashtable parameter $DomainsToBeIndexed
+You must specify the domain name and the indexNow key in the input hashtable parameter DomainsToBeIndexed
 
 .PARAMETER DomainsToBeIndexed
 Specifies the hashtable consisting of the domain name with the associated IndexNow key
@@ -20,9 +20,10 @@ Log information.
 
 .EXAMPLE
 
-PS> CherryPick.ps1 -User <Azure Devops user email> -Organization <organization> -Project <project> -Pat <personal access token> -LocalGitFolder "C:\Users\<user>\source\repos\" -DestinationBranches @("release/1.0","release/1.5","release/2.0","master") -PullRequest 12345
-
-- Returns the series of powershell scripts
+PS> IndexNowIndexWebsites.ps1 -DomainsToBeIndexed @{
+	"<domain 1>" = '<index now key 1>'
+	"<domain 2>" = '<index now key 2>'
+}
 
 .LINK
 
@@ -35,7 +36,7 @@ https://github.com/fededim/Fededim.Resources/blob/master/LICENSE.txt
 #>
 [CmdletBinding()]
 param(
-	[ValidateNotNullOrEmpty()] [Alias('u')] [Hashtable] $DomainsToBeIndexed = @{
+	[ValidateNotNullOrEmpty()] [Alias('d')] [Hashtable] $DomainsToBeIndexed = @{
 		"www.fdm-in.freeddns.org" = '4c6a24f07fb641b19a43d02ce5a35738'
 		"fededim.github.io" = 'b90e22c7680d4d4fbf051bc68004064e'
 	}
@@ -52,7 +53,7 @@ foreach ($keyValuePair in $DomainsToBeIndexed.GetEnumerator()) {
 	catch {}
 	
 	if ($? -ne $true -or $keyValuePair.Value -ne $response.Content) {
-		Write-Host "Domain $($keyValuePair.Name) IndexNowKey $($keyValuePair.Value) is missing or different at location $indexNowKeyLocation!`n"
+		Write-Host "Domain $($keyValuePair.Name) IndexNowKey $($keyValuePair.Value) is missing or different at location $indexNowKeyLocation!`n" -ForegroundColor Red
 		Continue
 	}
 
@@ -63,7 +64,7 @@ foreach ($keyValuePair in $DomainsToBeIndexed.GetEnumerator()) {
 	catch {}
 
 	if ($? -ne $true -or $response.StatusCode -ge 300 -or [System.String]::IsNullOrEmpty($response.Content)) {
-		Write-Host "Domain $($keyValuePair.Name) sitemap at $sitemapLocation is missing or broken!`n"
+		Write-Host "Domain $($keyValuePair.Name) sitemap at $sitemapLocation is missing or broken!`n" -ForegroundColor Red
 		Continue
 	}
 
@@ -80,12 +81,12 @@ foreach ($keyValuePair in $DomainsToBeIndexed.GetEnumerator()) {
 	$response = (Invoke-WebRequest -Method Post -Uri "https://api.indexnow.org/indexnow" -Body $indexNowBody -ContentType "application/json")
 
 	if ($? -ne $true -or $response.StatusCode -ge 300) {
-		Write-Host "Error while indexing Domain $($keyValuePair.Name) SearchEngine $searchEngine StatusCode $($response.StatusCode) Content $($response.Content)"
+		Write-Host "Error while indexing Domain $($keyValuePair.Name) SearchEngine $searchEngine StatusCode $($response.StatusCode) Content $($response.Content)" -ForegroundColor Red
 		$indexNowBody
 		$response
 	}
 	else {
-		Write-Host "Domain $($keyValuePair.Name) submitted to IndexNow successfully!`nIndexed pages:"
+		Write-Host "Domain $($keyValuePair.Name) submitted to IndexNow successfully!`nIndexed pages:" -ForegroundColor Green
 		$pages
 		Write-Host "`n"
 	}
