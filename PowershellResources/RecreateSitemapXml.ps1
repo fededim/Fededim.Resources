d6200d17870f9e062d4ea8180445bc396f8dbfb5
Federico Di Marco <fededim@gmail.com>
2025-05-21 17:24:57 +0200

    minor change

--- PowershellResources/RecreateSitemapXml.ps1
+++ PowershellResources/RecreateSitemapXml.ps1
@@ -42,7 +42,7 @@ https://github.com/fededim/Fededim.Resources/blob/master/LICENSE.txt
 param(
 	[ValidateNotNullOrEmpty()] [Alias('f')] [String] $RootFolder,
 	[ValidateNotNullOrEmpty()] [Alias('d')] [String] $Domain,
-	[ValidateNotNullOrEmpty()] [Alias('r')] [String] $FileMatchingRegex = "(\.htm(l)|\.pdf)"
+	[ValidateNotNullOrEmpty()] [Alias('r')] [String] $FileMatchingRegex = "(\.htm(l)|\.pdf|\.md)"
 )
 
 $domainUri = New-Object System.Uri($Domain)
